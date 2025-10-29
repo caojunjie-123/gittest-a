e7db47276007c6763a73eb825ff9920bdb2f1abb
caojunjie <caojunjie1459@link.tyut.edu.cn>
2025-10-29 10:24:00 +0800

    second commit

--- test2.py
+++ test2.py
@@ -2,8 +2,7 @@ print("hello,world!")
 print("hello")
 print("world")
 #开发商品货架模块
-<<<<<<< Updated upstream
 #开发商品货架模块bug已经修复
-=======
 #开发货架商品识别模块1/2
->>>>>>> Stashed changes
+print("a,b,c")
+print("d,e,f")
